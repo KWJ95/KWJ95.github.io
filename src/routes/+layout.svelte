aa00f0315771c8c8354e23511099297133b2d589
KWJ-EVDG <weijie.koo@evdg.cc>
2023-08-10 18:15:12 +0800

    showcase update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script>
-    import {base} from '$app/paths';
+	import { base } from '$app/paths';
+	import { page } from '$app/stores';
 </script>
 
 <nav>
-    <a href="{base}/">Home</a>
-    <a href="{base}/about">About</a>
+	<a href="{base}/" aria-current={$page.url.pathname === '/'}>Home</a>
+	<a href="{base}/about" aria-current={$page.url.pathname === '/about'}>About</a>
+	<a href="{base}/showcase" aria-current={$page.url.pathname === '/showcase'}>Showcase</a>
 </nav>
 
 <slot />
---
+++ src/routes/showcase/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	import Card from './Card.svelte';
+	import { time, elapsed } from './stores';
+
+	const timeZones = ['GMT', 'Europe/London', 'Asia/Tokyo', 'Asia/Singapore'];
+	const formatter = (tmz = 'GMT') =>
+		new Intl.DateTimeFormat('en', {
+			hour12: true,
+			hour: 'numeric',
+			minute: '2-digit',
+			second: '2-digit',
+			timeZone: tmz
+		});
+</script>
+
+<!-- <h1>The time is {formatter.format($time)}</h1> -->
+
+{#each timeZones as timeZone}
+	<h3>The time is {formatter(timeZone).format($time)} @ {timeZone}</h3>
+{/each}
+
+<p>
+	This page has been open for
+	{$elapsed}
+	{$elapsed === 1 ? 'second' : 'seconds'}
+</p>
+
+<Card />
