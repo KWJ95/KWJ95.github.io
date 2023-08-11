8afc981bcd7092757135d998260c58d68d2add3e
KWJ95 <50200917+KWJ95@users.noreply.github.com>
2023-08-11 23:18:00 +0800

    bug fix +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,9 +5,9 @@
 </script>
 
 <nav>
-	<a href="{base}/" aria-current="{&#x24;page.url.pathname" =="=" ' '}>Home</a>
-	<a href="{base}/about" aria-current="{&#x24;page.url.pathname" =="=" ' about'}>About</a>
-	<a href="{base}/showcase" aria-current="{&#x24;page.url.pathname" =="=" ' showcase'}>Showcase</a>
+	<a href="{base}/" aria-current="{$page.url.pathname == '/'}">Home</a>
+	<a href="{base}/about" aria-current="{$page.url.pathname == '/about'}">About</a>
+	<a href="{base}/showcase" aria-current="{$page.url.pathname == '/showcase'}">Showcase</a>
 </nav>
 
 <slot></slot>
