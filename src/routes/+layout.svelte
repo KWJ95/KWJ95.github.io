8d9a22980f8ab9689177d67acd6e3d52acf12fb6
KWJ95 <50200917+KWJ95@users.noreply.github.com>
2023-08-11 23:13:35 +0800

    add skeleton UI library

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,13 @@
 <script>
-	import { base } from '$app/paths';
-	import { page } from '$app/stores';
+	import "../app.postcss";
+    import { base } from '$app/paths';
+    import { page } from '$app/stores';
 </script>
 
 <nav>
-	<a href="{base}/" aria-current={$page.url.pathname === '/'}>Home</a>
-	<a href="{base}/about" aria-current={$page.url.pathname === '/about'}>About</a>
-	<a href="{base}/showcase" aria-current={$page.url.pathname === '/showcase'}>Showcase</a>
+	<a href="{base}/" aria-current="{&#x24;page.url.pathname" =="=" ' '}>Home</a>
+	<a href="{base}/about" aria-current="{&#x24;page.url.pathname" =="=" ' about'}>About</a>
+	<a href="{base}/showcase" aria-current="{&#x24;page.url.pathname" =="=" ' showcase'}>Showcase</a>
 </nav>
 
-<slot />
+<slot></slot>
