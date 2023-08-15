acc9bb3a217a7b1010b2a80e9e3574e6988b7615
KWJ-EVDG <weijie.koo@evdg.cc>
2023-08-15 18:13:29 +0800

    table and navigation update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,37 +1,24 @@
 <script>
-	import '@skeletonlabs/skeleton/styles/skeleton.css';
-	import { LightSwitch } from '@skeletonlabs/skeleton';
-	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 	import '../app.postcss';
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 	import { base } from '$app/paths';
+	import { LightSwitch, TabGroup, TabAnchor, Tab } from '@skeletonlabs/skeleton';
 	import { page } from '$app/stores';
 </script>
 
-<nav
-	class="nav absolute flex flex-col shadow-md w-[50%] h-full px-4 right-0 md:static md:h-auto md:w-auto md:flex-row md:shadow-none duration-500"
+<TabGroup
+	justify="justify-center"
+	flex="flex-1 lg:flex-none"
 >
-	<button class="close-nav relative top-0 right-0 text-right text-3xl md:hidden">
-		<i class="fa-solid fa-xmark" />
-	</button>
-	<a
-		href="{base}/"
-		aria-current={$page.url.pathname == '/'}
-		class="nav-link text-2xl mb-2 md:mr-4 hover:text-blue-400">Home</a
+	<TabAnchor href="{base}/" selected={$page.url.pathname == '/'}>Home</TabAnchor>
+	<TabAnchor href="{base}/about" selected={$page.url.pathname == '/about/'}>About</TabAnchor>
+	<TabAnchor href="{base}/showcase" selected={$page.url.pathname == '/showcase/'}>Showcase</TabAnchor
 	>
-	<a
-		href="{base}/about"
-		aria-current={$page.url.pathname == '/about'}
-		class="nav-link text-2xl mb-2 md:mr-4 hover:text-blue-400">About</a
-	>
-	<a
-		href="{base}/showcase"
-		aria-current={$page.url.pathname == '/showcase'}
-		class="nav-link text-2xl mb-2 md:mr-4 hover:text-blue-400">Showcase</a
-	>
-	<span class="inline-block align-basline">
-		<LightSwitch/>
-	</span>
-</nav>
+	<Tab group="" name="lightswitch" value="null" active="not-active">
+		<LightSwitch />
+	</Tab>
+</TabGroup>
 <body>
 	<slot />
 </body>
--- src/routes/showcase/+page.svelte
+++ src/routes/showcase/+page.svelte
@@ -1,7 +1,9 @@
-<script>
+<script lang="ts">
 	import Card from './Card.svelte';
 	import MasonryGallery from './MasonryGallery.svelte';
 	import { time, elapsed } from './stores';
+	import { Table, tableMapperValues } from '@skeletonlabs/skeleton';
+	import type { TableSource } from '@skeletonlabs/skeleton';
 
 	const timeZones = ['GMT', 'Europe/London', 'Asia/Tokyo', 'Asia/Singapore'];
 	const formatter = (tmz = 'GMT') =>
@@ -12,14 +14,41 @@
 			second: '2-digit',
 			timeZone: tmz
 		});
+
+	const timeZonesTable = [
+		{ country: 'GMT', t: formatter().format($time) },
+		{ country: 'Europe/London', t: formatter('Europe/London').format($time) },
+		{ country: 'Asia/Tokyo', t: formatter('Asia/Tokyo').format($time) },
+		{ country: 'Asia/Singapore', t: formatter('Asia/Singapore').format($time) }
+	];
+
+	const tableTimeZone: TableSource = {
+		head: ['Current Time', 'Country/Timezone'],
+		body: tableMapperValues(timeZonesTable, ['t', 'country'])
+	};
+
+	console.table(timeZones);
 </script>
 
 <!-- <h1>The time is {formatter.format($time)}</h1> -->
+<table class="table-auto">
+	<thead>
+		<tr>
+			<th>Current Time</th>
+			<th>Country/Timezone</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each timeZones as timeZone}
+			<tr>
+				<td>{formatter(timeZone).format($time)}</td>
+				<td>{timeZone}</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
 
-{#each timeZones as timeZone}
-	<h3>The time is {formatter(timeZone).format($time)} @ {timeZone}</h3>
-{/each}
-
+<Table source={tableTimeZone} class="w-[50%] justify-center" />
 <p>
 	This page has been open for
 	{$elapsed}
@@ -28,7 +57,7 @@
 
 <Card />
 
-<hr class="my-5"/>
+<hr class="my-5" />
 <h1 class="h1">Demo for masonry gallery</h1>
-<br>
-<MasonryGallery/>
+<br />
+<MasonryGallery />
