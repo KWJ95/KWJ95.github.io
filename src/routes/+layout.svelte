be00a4df3e532d43a04586a686917ed5cad80365
KWJ95 <50200917+KWJ95@users.noreply.github.com>
2025-05-03 11:53:13 +0800

    migration and rewrite project

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,10 @@
-<script>
-	import '../app.postcss';
-	import '@skeletonlabs/skeleton/styles/skeleton.css';
-	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
-	import { base } from '$app/paths';
-	import { LightSwitch, TabGroup, TabAnchor, Tab } from '@skeletonlabs/skeleton';
-	import { page } from '$app/stores';
-	import { Modal } from '@skeletonlabs/skeleton';
+<script lang="ts">
+	import { i18n } from "$lib/i18n";
+	import { ParaglideJS } from "@inlang/paraglide-sveltekit";
+	import '../app.css';
+	let { children } = $props();
 </script>
 
-<Modal />
-
-<TabGroup justify="justify-center" flex="flex-1 lg:flex-none">
-	<TabAnchor href="{base}/" selected={$page.url.pathname == '/'}>Home</TabAnchor>
-	<TabAnchor href="{base}/about" selected={$page.url.pathname == '/about/'}>About</TabAnchor>
-	<TabAnchor href="{base}/showcase" selected={$page.url.pathname == '/showcase/'}
-		>Showcase</TabAnchor
-	>
-	<Tab group="" name="lightswitch" value="null" active="not-active">
-		<LightSwitch />
-	</Tab>
-</TabGroup>
-<body>
-	<slot />
-</body>
+<ParaglideJS {i18n}>
+	{@render children()}
+</ParaglideJS>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+import { defineConfig } from "vitest/config";
+import { paraglide } from "@inlang/paraglide-sveltekit/vite";
 import { sveltekit } from '@sveltejs/kit/vite';
-import { enhancedImages } from '@sveltejs/enhanced-img';
-import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [enhancedImages(), sveltekit()]
+    plugins: [sveltekit(), paraglide({
+        project: "./project.inlang",
+        outdir: "./src/lib/paraglide"
+    })],
+
+    test: {
+        include: ['src/**/*.{test,spec}.{js,ts}']
+    }
 });
